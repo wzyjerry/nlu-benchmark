8ff647ed91f378247fac5924a3b50717322d410e
WzyJerry <wzy950507@163.com>
2019-01-24 21:41:48 +0800

    entity & package

---
+++ 2017-06-custom-intent-engines/gen_entity.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+intents = [
+  'AddToPlaylist',
+  'BookRestaurant',
+  'GetWeather',
+  'PlayMusic',
+  'RateBook',
+  'SearchCreativeWork',
+  'SearchScreeningEvent'
+]
+
+template = 'train_%s.json'
+
+entities = {}
+for intent in intents:
+  with open(os.path.join(intent, template % intent), 'r', encoding='utf-8') as fin:
+    data = json.load(fin)
+    print(intent)
+    for item in data[intent]:
+      for seg in item['data']:
+        if 'entity' in seg:
+          entities.setdefault(seg['entity'], set())
+          entities[seg['entity']].add(seg['text'].strip())
+
+OUT_DIR = 'out/entities'
+for kind in entities:
+  with open(os.path.join(OUT_DIR, kind), 'w', encoding='utf-8') as fout:
+    for entity in entities[kind]:
+      fout.write('%s\n' % entity)
---
+++ 2017-06-custom-intent-engines/gen_packages.py
@@ -0,0 +1,342 @@
+entity_list = [{
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b873c4952fdf7150e306",
+	"name": "album"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b87cc4952fdf7150e307",
+	"name": "artist"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b885c4952fdf7150e308",
+	"name": "best_rating"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b88dc4952fdf7150e309",
+	"name": "city"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b896c4952fdf7150e30a",
+	"name": "condition_description"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b89fc4952fdf7150e30b",
+	"name": "condition_temperature"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8a7c4952fdf7150e30c",
+	"name": "country"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8adc4952fdf7150e30d",
+	"name": "cuisine"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8c6c4952fdf7150e30e",
+	"name": "current_location"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8d0c4952fdf7150e30f",
+	"name": "entity_name"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8dac4952fdf7150e310",
+	"name": "facility"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8e2c4952fdf7150e311",
+	"name": "genre"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8eac4952fdf7150e312",
+	"name": "geographic_poi"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8f2c4952fdf7150e313",
+	"name": "location_name"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b8fbc4952fdf7150e314",
+	"name": "movie_name"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b904c4952fdf7150e315",
+	"name": "movie_type"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b90cc4952fdf7150e316",
+	"name": "music_item"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b913c4952fdf7150e317",
+	"name": "object_location_type"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b91bc4952fdf7150e318",
+	"name": "object_name"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b923c4952fdf7150e319",
+	"name": "object_part_of_series_type"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b92ac4952fdf7150e31a",
+	"name": "object_select"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b932c4952fdf7150e31b",
+	"name": "object_type"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b938c4952fdf7150e31c",
+	"name": "party_size_description"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b941c4952fdf7150e31d",
+	"name": "party_size_number"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b949c4952fdf7150e31e",
+	"name": "playlist"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b951c4952fdf7150e31f",
+	"name": "playlist_owner"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b959c4952fdf7150e320",
+	"name": "poi"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b961c4952fdf7150e321",
+	"name": "rating_unit"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b969c4952fdf7150e322",
+	"name": "rating_value"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b971c4952fdf7150e323",
+	"name": "restaurant_name"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b97ac4952fdf7150e324",
+	"name": "restaurant_type"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b981c4952fdf7150e325",
+	"name": "served_dish"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b988c4952fdf7150e326",
+	"name": "service"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b98fc4952fdf7150e327",
+	"name": "sort"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b997c4952fdf7150e328",
+	"name": "spatial_relation"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b99fc4952fdf7150e329",
+	"name": "state"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b9a6c4952fdf7150e32a",
+	"name": "timeRange"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b9aec4952fdf7150e32b",
+	"name": "track"
+}, {
+	"agent_id": "5c49b83cc4952fdf7150e305",
+	"description": "",
+	"id": "5c49b9b6c4952fdf7150e32c",
+	"name": "year"
+}]
+entity_map = {x['name']: x['id'] for x in entity_list}
+
+import os
+import json
+
+def auto_rules(query_list):
+  d = {}
+  for item in query_list:
+    d.setdefault(item['feature'], [])
+    d[item['feature']].append(item)
+  count = 0
+  rules = []
+  for slots, result_list in d.items():
+    count += 1
+    rule = {
+      'name': 'rule' + str(count),
+      'nodes': []
+    }
+    nodes = rule['nodes']
+    dropout = [0.0 for _ in range(len(slots) + 1)]
+    content = [set() for _ in range(len(slots) + 1)]
+    for result in result_list:
+      for i in range(len(result['sp'])):
+        sp = result['sp'][i]
+        if len(result['token'][sp[0]:sp[1]]) > 0:
+          content[i].add(' '.join(result['token'][sp[0]:sp[1]]))
+        else:
+          dropout[i] += 1
+    nodes.append({
+      'type': 'content',
+      'value': ' | '.join(list(content[0])),
+      'dropout': dropout[0] / len(result_list)
+    })
+    for i in range(len(slots)):
+      nodes.append({
+        'type': 'entity',
+        'value': slots[i],
+        'slot': slots[i],
+        'dropout': 0.0
+      })
+      nodes.append({
+        'type': 'content',
+        'value': ' | '.join(list(content[i+1])),
+        'dropout': dropout[i+1] / len(result_list)
+      })
+    rules.append(rule)
+  return rules
+
+def make_package(
+  entitymap,
+  rules,
+  name='quantity',
+  weight=0.01):
+  package = {
+    'name': name,
+    'tree': {
+      'intent': name,
+      'type': 'intent',
+      'weight': weight,
+      'children': [
+        {
+          "type": "holder"
+        }
+      ]
+    }
+  }
+  children = package['tree']['children']
+  for rule in rules:
+    child = {
+      'type': 'order',
+      'name': rule['name'],
+      'dropout': 0,
+      'children': []
+    }
+    children.append(child)
+    nodes = child['children']
+    for node in rule['nodes']:
+      if node['dropout'] < 1.0:
+        if node['type'] == 'content':
+          content = node['value'].split(' | ')
+          nodes.append({
+            'content': content,
+            'type': 'content',
+            'cut': 0,
+            'name': content[0],
+            'dropout': node['dropout']
+          })
+        elif node['type'] == 'entity':
+          nodes.append({
+            'type': 'content',
+            'cut': 0,
+            'isSlot': True,
+            'name': '<%s>' % node['value'],
+            'entity': entitymap[node['value']],
+            'slot': node['slot']
+          })
+  return json.dumps(package)
+
+intents = [
+  'AddToPlaylist',
+  'BookRestaurant',
+  'GetWeather',
+  'PlayMusic',
+  'RateBook',
+  'SearchCreativeWork',
+  'SearchScreeningEvent'
+]
+
+template = 'train_%s.json'
+
+def make_query_list(intent):
+  queries = []
+  with open(os.path.join(intent, template % intent), 'r', encoding='utf-8') as fin:
+    data = json.load(fin)
+    print(intent)
+    for item in data[intent]:
+      feature = []
+      token = []
+      sp = []
+      st = 0
+      for seg in item['data']:
+        text = seg['text'].strip()
+        if text == '':
+          continue
+        token.append(text)
+        if 'entity' in seg:
+          feature.append(seg['entity'])
+          sp.append((st, len(token) - 1))
+          st = len(token)
+      sp.append((st, len(token)))
+      queries.append({
+        'feature': tuple(feature),
+        'token': token,
+        'sp': sp
+      })
+  return queries
+
+OUT_DIR = 'out/packages'
+
+for intent in intents:
+  with open(os.path.join(OUT_DIR, intent), 'w', encoding='utf-8') as fout:
+    fout.write(make_package(
+      entity_map,
+      auto_rules(make_query_list(intent)),
+      name=intent,
+      weight=int(100 / 7) / 100.0))
